4d94377082b67683aa9ebc4ecd3d239e821f4dc8
Quirin <quirin.koch@gmx.net>
2017-02-24 20:19:14 +0100

    player.html

--- codenames/gulp/tasks/watch.js
+++ codenames/gulp/tasks/watch.js
@@ -6,7 +6,8 @@ gulp.task('watch', function () {
     browserSync.init({
         notify: false,
         server: {
-            baseDir: "app"
+            baseDir: "app",
+            index: "player.html"
         }
     });
 
